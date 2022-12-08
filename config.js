f000c23e67c054f140383004de300a90eba56ae9
SAM-OCHU <118100214+SAM-OCHU@users.noreply.github.com>
2022-12-08 01:49:09 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,48 @@
+const fs = require('fs-extra')
+if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
+
+
+//═══════[Required Variables]════════\\
+global.owner = process.env.OWNER_NUMBER.split(",")
+global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
+global.port= process.env.PORT || 5000
+global.email = 'samochubrand@gmail.com'
+global.github = 'https://github.com/SAM-OCHU/Kibalanga-Bot'
+global.location = 'Sultanpur IN'
+global.gurl = 'https://instagram.com/' // add your username
+global.sudo = process.env.SUDO || '255678101729'
+global.devs = '919628516236';
+global.website = 'https://github.com/SAM-OCHU/Kibalanga-Bot' //wa.me/+2550000000000
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
+module.exports = {
+  botname: process.env.BOT_NAME || 'Kibalanga Bot',
+  ownername:process.env.OWNER_NAME || 'SamPandey001',
+  sessionName: process.env.SESSION_ID || 'PUT-HERE',
+  author: process.env.PACK_INFO.split(";")[0] || 'author', 
+  packname: process.env.PACK_INFO.split(";")[1] || 'Name',
+  autoreaction: process.env.AUTO_REACTION || 'on',
+  antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
+  alwaysonline: process.env.ALWAYS_ONLINE || 'false',
+  antifake : process.env.FAKE_COUNTRY_CODE || '',
+  readmessage: process.env.READ_MESSAGE || false,
+  HANDLERS: process.env.PREFIX || ['.'],
+  warncount : process.env.WARN_COUNT || 3,
+  disablepm: process.env.DISABLE_PM || "flase",
+  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
+  antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
+  antilinkaction: process.env.ANTILINK_ACTION || 'remove',
+  BRANCH: 'main',
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'put-key-here',
+  VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
+  LANG: process.env.THEME|| 'SECKTOR',
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
+};
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+    delete require.cache[file]
+	require(file)
+})
